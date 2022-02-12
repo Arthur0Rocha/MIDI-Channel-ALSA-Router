333e89998ee3d78eca2d5483b3b33a87ea6a36ce
Arthur0Rocha <ar_d_rocha@hotmail.com>
2022-02-12 14:22:47 -0300

    changed to a single IO port

--- src/alsainterface.c
+++ src/alsainterface.c
@@ -1,17 +1,16 @@
 #include "headers/alsainterface.h"
 
 static snd_seq_t *seq_handle;
-static int in_port, out_port;
+static int port;
 
 void midi_open() {
     snd_seq_open(&seq_handle, "default", SND_SEQ_OPEN_DUPLEX, 0);
     snd_seq_set_client_name(seq_handle, "Arthur MIDI HUB");
-    in_port = snd_seq_create_simple_port(seq_handle, "input",
-                    SND_SEQ_PORT_CAP_WRITE|SND_SEQ_PORT_CAP_SUBS_WRITE,
+    port = snd_seq_create_simple_port(seq_handle, "IO",
+                    SND_SEQ_PORT_CAP_READ|SND_SEQ_PORT_CAP_WRITE|
+                    SND_SEQ_PORT_CAP_SUBS_READ|SND_SEQ_PORT_CAP_SUBS_WRITE,
                     SND_SEQ_PORT_TYPE_APPLICATION);
-    out_port = snd_seq_create_simple_port(seq_handle, "output",
-                    SND_SEQ_PORT_CAP_READ|SND_SEQ_PORT_CAP_SUBS_READ,
-                    SND_SEQ_PORT_TYPE_APPLICATION);
+    
 }
 
 snd_seq_event_t *midi_read() {
@@ -21,7 +20,7 @@ snd_seq_event_t *midi_read() {
 }
 
 void midi_send(snd_seq_event_t *ev) {
-    snd_seq_ev_set_source(ev, out_port);
+    snd_seq_ev_set_source(ev, port);
     snd_seq_ev_set_subs(ev);
     snd_seq_ev_set_direct(ev);
     snd_seq_event_output_direct(seq_handle, ev);
