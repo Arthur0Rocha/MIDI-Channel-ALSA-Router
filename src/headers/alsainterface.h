4eef25ff1ffc6826625a9fe4a63859e4fd491693
Arthur0Rocha <ar_d_rocha@hotmail.com>
2022-02-19 00:22:32 -0300

    updating alsa interface header file

--- src/headers/alsainterface.h
+++ src/headers/alsainterface.h
@@ -7,5 +7,8 @@ void midi_open();
 snd_seq_event_t *midi_read();
 void midi_send(snd_seq_event_t *ev);
 void midi_updt_channel(snd_seq_event_t *ev, int channel);
+void connectKROME(uint8_t input, uint8_t output);
+void connectVORTEX();
+void connectQSYNTH();
 
 #endif // _ARTHUR_ALSA_INTERFACE
